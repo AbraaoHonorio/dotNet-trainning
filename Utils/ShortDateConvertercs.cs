37a123fcfaba72077fe9d756551a9e6f1deef63c
Abraão Állysson dos Santos Honório <abraao.allyson@eng.ci.ufpb.br>
2018-12-26 14:53:45 -0300

    Fazendo API aceitar data no formato `dd/MM/yyyy`

---
+++ Utils/ShortDateConvertercs.cs
@@ -0,0 +1,50 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Threading.Tasks;
+
+#pragma warning disable CS1591 // O comentário XML ausente não foi encontrado para o tipo ou membro visível publicamente
+
+namespace nfse_webApi.Utils
+{
+    public class ShortDateConvertercs : JsonConverter
+    {
+        public override bool CanConvert(Type objectType)
+        {
+            return objectType == typeof(DateTime);
+        }
+
+        public override object ReadJson(Newtonsoft.Json.JsonReader reader, Type objectType, object existingValue, Newtonsoft.Json.JsonSerializer serializer)
+        {
+            return DateTime.ParseExact((string)reader.Value, "dd/MM/yyyy", CultureInfo.InvariantCulture);
+        }
+
+        public override void WriteJson(Newtonsoft.Json.JsonWriter writer, object value, Newtonsoft.Json.JsonSerializer serializer)
+        {
+            DateTime d = (DateTime)value;
+            writer.WriteValue(d.ToString("dd/MM/yyyy"));
+        }
+    }
+
+    public class ShortDateMothYearsConvertercs : JsonConverter
+    {
+        public override bool CanConvert(Type objectType)
+        {
+            return objectType == typeof(DateTime);
+        }
+
+        public override object ReadJson(Newtonsoft.Json.JsonReader reader, Type objectType, object existingValue, Newtonsoft.Json.JsonSerializer serializer)
+        {
+            return DateTime.ParseExact((string)reader.Value, "MM/yyyy", CultureInfo.InvariantCulture);
+        }
+
+        public override void WriteJson(Newtonsoft.Json.JsonWriter writer, object value, Newtonsoft.Json.JsonSerializer serializer)
+        {
+            DateTime d = (DateTime)value;
+            writer.WriteValue(d.ToString("MM/yyyy"));
+        }
+    }
+}
+
