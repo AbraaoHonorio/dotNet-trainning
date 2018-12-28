0fd89322caf05ca022185cefa91fd7a983cb1127
abraaoHonorio <abraao.honorio@eng.ci.ufpb.br>
2018-12-28 09:42:35 -0300

    tratamento de data para api

---
+++ Utils/DateTimeConvertecs.cs
@@ -0,0 +1,33 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Threading.Tasks;
+
+#pragma warning disable CS1591 // O comentário XML ausente não foi encontrado para o tipo ou membro visível publicamente
+
+namespace nfse_webApi.Utils
+{
+    public class DateTimeConvertecs : JsonConverter
+    {
+        public override bool CanConvert(Type objectType)
+        {
+            return objectType == typeof(DateTime);
+        }
+
+        public override object ReadJson(Newtonsoft.Json.JsonReader reader, Type objectType, object existingValue, Newtonsoft.Json.JsonSerializer serializer)
+        {
+            return DateTime.ParseExact((string)reader.Value, "dd/MM/yyyy HH:mm", CultureInfo.InvariantCulture);
+        }
+
+        public override void WriteJson(Newtonsoft.Json.JsonWriter writer, object value, Newtonsoft.Json.JsonSerializer serializer)
+        {
+            DateTime d = (DateTime)value;
+            writer.WriteValue(d.ToString("dd/MM/yyyy HH:mm"));
+        }
+    }
+
+
+    
+}
