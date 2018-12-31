e259a5104f2297f457cc72623941082505042021
abraaohonorio <abraao.honorio@eng.ci.ufpb.br>
2018-12-31 11:08:20 -0300

    adicionando o repository pattern

---
+++ Project 03 - API/TesteStore/Domain/Repositories/ICustomerRepository.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidator;
+using System;
+using TesteStore.Domain.StoreContext.Commands.CustomerCommands.Inputs;
+using TesteStore.Domain.StoreContext.Commands.CustomerCommands.Outputs;
+using TesteStore.Domain.StoreContext.Entities;
+using TesteStore.Domain.StoreContext.ValueObjects;
+
+namespace TesteStore.Domain.StoreContext.Repositories
+{
+    public interface ICustomerRepository
+    {
+        bool CheckDocument(string document);
+
+        bool CheckEmail(string email);
+
+        void Save(Customer customer);
+    }
+}
+
