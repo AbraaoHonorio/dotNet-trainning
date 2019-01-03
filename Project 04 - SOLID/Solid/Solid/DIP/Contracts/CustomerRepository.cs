eba23141ecf75d7c752cdc231d9d71406ef79527
abraaohonorio <abraao.honorio@eng.ci.ufpb.br>
2019-01-03 12:54:06 -0300

    Esse reposíorio visa aprender/ensinar sobre o SOLID

---
+++ Project 04 - SOLID/Solid/Solid/DIP/Contracts/CustomerRepository.cs
@@ -0,0 +1,14 @@
+﻿using Solid.Models;
+using System;
+
+
+namespace Solid.DIP.Contracts
+{
+    public class CustomerRepository : ICustomerRepository
+    {
+        public void Save(Customer customer)
+        {
+            Console.WriteLine(" ->>>" + customer.ToString());
+        }
+    }
+}
